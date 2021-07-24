15495db20032034d4c43e77b49831f86f6773d5c
Yusuf Gultekin <yusufgultekin406@gmail.com>
2021-07-24 00:58:25 -0400

    added more text

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,6 +1,13 @@
 public class GitCool {
     // Ay im learning git and I am proud for catching up on my work
 
-    //Added my github account under intelliJ
-    //make sure to click apply and and save
+    // Added my github account under intelliJ
+    // make sure to click apply and and save
+
+    // successfully shared project
+    // you can only share project 1 time per project
+    // created remote repository under github account
+    // created a link between local repo and remote repo
+    // push local commits to remote repo
+
 }
