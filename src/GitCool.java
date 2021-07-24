e5226f23f1dd48205868b640c768af6daeee4ef4
Yusuf Gultekin <yusufgultekin406@gmail.com>
2021-07-24 00:56:12 -0400

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,3 +1,6 @@
 public class GitCool {
     // Ay im learning git and I am proud for catching up on my work
+
+    //Added my github account under intelliJ
+    //make sure to click apply and and save
 }
